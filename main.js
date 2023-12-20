9cb4e573d114af01f67a86c4dc36c923b1b2f991
Douglas Winter <douglaswinter75@gmail.com>
2023-12-20 10:43:54 -0300

    Commit semi final

--- main.js
+++ main.js
@@ -9,5 +9,16 @@ $('form').on('submit', function(e) {
     const novaLinha = $('<li></li>');
         $(`<li> ${novaTarefa} </li>`).appendTo(novaLinha);
         $(novaLinha).appendTo('ul');
-    })    
-})        
+        $('#nova-tarefa').val('');
+    })
+const Tabelaraiz = document.querySelector('ul');
+
+Tabelaraiz.addEventListener('click', function(e) {
+    if(e.target.classList.contains('checked')) {
+        e.target.classList.remove('checked');
+    } else {
+        e.target.classList.add('checked');
+    }
+})
+
+});     
